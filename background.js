cd968862bd1700545a2c6104d61e39f887554914
Elaina Martineau <elainamartineau@gmail.com>
2021-04-12 20:22:53 -0600

    Initial commit

---
+++ background.js
@@ -0,0 +1,87 @@
+let tab_info = new Map();
+
+function extract(url, id) {
+    for (let extractor of extractors) {
+        const result = extractor.url_regexp.exec(url);
+        if (result) {
+            let content_id, media_type;
+            if (tab_info.has(id)) {
+                [content_id, media_type] = tab_info.get(id);
+            } else if (extractor.url_extractor) {
+                [content_id, media_type] = extractor.url_extractor?.(result);
+            }
+
+            return {
+                name: extractor.name,
+                channel_id: extractor.channel_id,
+                content_id: content_id,
+                media_type: media_type
+            }
+        }
+    }
+}
+
+function set_title(url, tabId) {
+    const info = extract(url, tabId);
+    if (info) {
+        browser.pageAction.setTitle({
+            tabId: tabId,
+            title: `${info.content_id ? `Play this ${info.media_type ? info.media_type + " " : ""}on your Roku's ${info.name} app` : `Open ${info.name} on your Roku`}`
+        });
+    }
+}
+
+browser.tabs.onUpdated.addListener(async (tabId, changeInfo, tab) => {
+    if (changeInfo.status === "loading" && await browser.pageAction.isShown({ tabId: tabId })) {
+        set_title(changeInfo.url, tabId);
+        tab_info.delete(tabId);
+    }
+}, {
+    properties: ["status"]
+});
+
+browser.tabs.onRemoved.addListener((tabId) => {
+    tab_info.delete(tabId)
+});
+
+let utf8decoder = new TextDecoder();
+browser.webRequest.onBeforeRequest.addListener(
+    (details) => {
+        const raw = details.requestBody?.raw?.[0]?.bytes;
+        if (raw) {
+            const info = utf8decoder.decode(raw);
+            if (info) {
+                const title_id = /"asin":"(\w+)"/.exec(info)?.[1];
+                if (title_id) {
+                    tab_info.set(details.tabId, [title_id, "episode"]);
+                    set_title(details.documentUrl, details.tabId);
+                }
+            }
+        }
+    },
+    {
+        urls: ["*://fls-na.amazon.com/1/aiv-web-player/1/OE"]
+    },
+    ["requestBody"]
+);
+
+browser.pageAction.onClicked.addListener(async (tab, on_click_data) => {
+    const info = extract(tab.url, tab.id);
+    const roku_ip = browser.storage.local.get("roku_ip");
+
+    if (!roku_ip) return;
+
+    let qs = [];
+    if (info.content_id) qs.push(`contentId=${info.content_id}`);
+    if (info.media_type) qs.push(`mediaType=${info.media_type}`);
+    qs = qs.join("&");
+
+    const response = await fetch(`http://${roku_ip}:8060/launch/${info.channel_id}${qs ? "?" + qs : ""}`,
+        {
+            headers: {
+                "cache-control": "no-cache"
+            },
+            method: "POST",
+        }
+    );
+});
---
+++ extractors.js
@@ -0,0 +1,69 @@
+function url_rxn(base, optional) {
+    optional = optional || "";
+    return new RegExp(`^https?://(?:\\w*\\.)${base}(?:${optional})?`, "i");
+}
+
+function url_rx(base, optional) {
+    return url_rxn(base.replace(/[.*+?^${}()|[\]\\]/g, '\\$&'), optional);
+}
+
+function extractor(media_type) {
+    return (result) => [result[1], media_type]
+}
+
+const extractors = [
+    {
+        channel_id: 12,
+        name: "Netflix",
+        url_regexp: url_rx("netflix.com", "/(title|watch)/(\\d+)"),
+        url_extractor: (result) => {
+            const media_type = result[1] == "title" ? "season" : "episode";
+            return [result[2], media_type];
+        }
+    },
+    {
+        channel_id: 13,
+        name: "Prime Video",
+        url_regexp: url_rxn("amazon\\.com/(?:Amazon-Video|gp/video/detail/(\\w+))"),
+        url_extractor: extractor("season"),
+    },
+    {
+        channel_id: 28,
+        name: "Pandora",
+        url_regexp: url_rx("pandora.com", "/station/play/(\\d+)"),
+        url_extractor: extractor("live")
+    },
+    {
+        channel_id: 837,
+        name: "YouTube",
+        url_regexp: url_rx("youtube.com", "/watch\\?v=(\\w+)"),
+        url_extractor: extractor("episode")
+    },
+    {
+        channel_id: 2285, name: "Hulu", url_regexp: url_rx("hulu.com"),
+    },
+    {
+        channel_id: 2595, name: "Crunchyroll", url_regexp: url_rx("crunchyroll.com"),
+    },
+    {
+        channel_id: 13535, name: "Plex", url_regexp: url_rx("plex.tv"),
+    },
+    {
+        channel_id: 13842,
+        name: "VUDU",
+        url_regexp: url_rx("vudu.com", "/.+/(\\d+)"),
+        url_extractor: extractor("movie")
+    },
+    {
+        channel_id: 291097, name: "Disney Plus", url_regexp: url_rx("disneyplus.com"),
+    },
+    {
+        channel_id: 591927, name: "Twoku", url_regexp: url_rx("twitch.tv")
+    },
+    {
+        channel_id: 593099,
+        name: "Peacock",
+        url_regexp: url_rx("peacocktv.com", "/watch/playback/vod/\\w+/([\\w-]+)"),
+        url_extractor: extractor("episode")
+    }
+];
